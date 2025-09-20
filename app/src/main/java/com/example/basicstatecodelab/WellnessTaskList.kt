35fd7a410364372400d743f7314407367c3258ac
axelr <axelrepeat@gmail.com>
2025-09-19 21:38:07 -0600

    La lógica empresarial ahora se refactoriza en ViewModel haciendo que todas las modificaciones sobrevivan a los de recomposición y configuración

--- app/src/main/java/com/example/basicstatecodelab/WellnessTaskList.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessTaskList.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.remember
 @Composable
 fun WellnessTaskList(
     list: List<WellnessTask>,
+    onCheckedTask: (WellnessTask, Boolean) -> Unit,
     onCloseTask: (WellnessTask) -> Unit,
     modifier: Modifier = Modifier,
 ) {
@@ -19,7 +20,11 @@ fun WellnessTaskList(
             items = list,
             key = { task -> task.id }
         ) { task ->
-            WellnessTaskItem(taskName = task.label, onClose = { onCloseTask(task) })
+            WellnessTaskItem(
+                taskName = task.label,
+                checked = task.checked,
+                onCheckedChange = { checked -> onCheckedTask(task, checked) },
+                onClose = { onCloseTask(task) })
         }
     }
 }
